f41040672270c9a2c5d4e29c025bbbe4f231886f
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-14 20:35:06 +0100

    fixed a lot of small tipos in the commands
    also make the cur_state work

--- create_vm.sh
+++ create_vm.sh
@@ -50,83 +50,86 @@ esac;
 
 echo "$CUR_STATE/$TOT_STATE - Creating a new logical volume for the VM"
 lvcreate -L $SIZE -n $NAME optiplex
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Creating a partition table on the new LV"
 parted /dev/optiplex/$NAME mktable msdos
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Creating a swap partition"
 parted /dev/optiplex/$NAME mkpartfs primary linux-swap 1 4G
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Creating a ext4 partition for /"
-parted /dev/optiplex/$NAME mkpartfs primary ext4 4G -1
-                                                                                CUR_STATE=$CUR_STATE+1
+parted /dev/optiplex/$NAME mkpartfs primary ext2 4G 100%
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Open LV"
-kpartx /dev/optiplex/template
-kpartx /dev/optiplex/$NAME
-                                                                                CUR_STATE=$CUR_STATE+1
+kpartx -a /dev/optiplex/template
+kpartx -a /dev/optiplex/$NAME
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Import template / into the new /"
-dd bs=4M if=/dev/mapper/optiplex-template2 of=/dev/mapper/optiplex-$NAME2
-                                                                                CUR_STATE=$CUR_STATE+1
+dd bs=4M if=/dev/mapper/optiplex-template2 of="/dev/mapper/optiplex-"$NAME"2"
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Fixing new / filesystem"
-e2fsck -f /dev/mapper/optiplex-$NAME2
-                                                                                CUR_STATE=$CUR_STATE+1
+e2fsck -fy "/dev/mapper/optiplex-"$NAME"2"
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Growing new / filesystem"
-resize2fs /dev/mapper/optiplex-$NAME2
-                                                                                CUR_STATE=$CUR_STATE+1
+resize2fs "/dev/mapper/optiplex-"$NAME"2"
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Mounting new filesystem"
 TEMP=`mktemp -d`
-mount /dev/optiplex/$NAME $TEMP
-                                                                                CUR_STATE=$CUR_STATE+1
+mount "/dev/mapper/optiplex-"$NAME"2" $TEMP
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Updating hostname information"
-sed -i 's/template/$NAME/' $TEMP/etc/hostname
-sed -i 's/template/$NAME/' $TEMP/etc/hosts
-                                                                                CUR_STATE=$CUR_STATE+1
+sed -i 's/template/'$NAME'/' $TEMP/etc/hostname
+sed -i 's/template/'$NAME'/' $TEMP/etc/hosts
+sed -i 's/template/'$NAME'/' $TEMP/etc/hosts
+# did it twice because template appear twice on a single line
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Unmounting new filesystem"
 umount $TEMP
 rm -r $TEMP
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Close LV"
 kpartx -d /dev/optiplex/template
 kpartx -d /dev/optiplex/$NAME
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Creating new configuration file"
-cat >> $CONFIG_PATH/$NAME.cfg < EOF
+echo "" > $CONFIG_PATH/$NAME.cfg
+cat >> $CONFIG_PATH/$NAME.cfg << EOF
 name = "$NAME"
 memory = 256
 disk = ["phy:/dev/optiplex/$NAME,xvda,w"]
 vif = [" "]
 bootloader = "pygrub"
 EOF
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Starting the new VM"
 xm create $CONFIG_PATH/$NAME.cfg
-                                                                                CUR_STATE=$CUR_STATE+1
+                                                                                let CUR_STATE=$CUR_STATE+1
                                                                                 if [ $? != 0 ]; then exit 1; fi
 
 echo "--- The END ---"

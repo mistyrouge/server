46aca6bfb736e97ba4803d8bf128a6137f469cd6
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-15 07:15:28 +0100

    volume group is now an optional argument
    ¡ Not tested !

--- create_vm.sh
+++ create_vm.sh
@@ -2,18 +2,20 @@
 
 usage(){
 cat << EOF
-usage : $0 -n NAME -s SIZE
+usage : $0 -n NAME -s SIZE [-h] [-v VOLUME_GROUP]
 
 OPTIONS:
 	-n	name of the new virtual machine
 	-s	size of the / partition on the new virtual machine (>=2G)
 	-h	print this help
+    -v  LVM Volume group to use
 EOF
 }
 
 NAME="undefined"
 SIZE="undefined"
 CONFIG_PATH="/home/dorian/vm_config"
+VG="optiplex"
 TOT_STATE=14
 CUR_STATE=1
 while getopts “hn:s:” OPTION
@@ -28,6 +30,9 @@ do
              ;;
          s)
              SIZE=$OPTARG
+             ;;
+         v)
+             VG=$OPTARG
              ;;
          ?)
              usage
@@ -53,40 +58,41 @@ next(STATUS){
 }
 
 echo "$CUR_STATE/$TOT_STATE - Creating a new logical volume for the VM"
-lvcreate -L $SIZE -n $NAME optiplex
+lvcreate -L $SIZE -n $NAME $VG
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating a partition table on the new LV"
-parted /dev/optiplex/$NAME mktable msdos
+parted /dev/$VG/$NAME mktable msdos
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating a swap partition"
-parted /dev/optiplex/$NAME mkpartfs primary linux-swap 1 4G
+parted /dev/$VG/$NAME mkpartfs primary linux-swap 1 4G
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating a ext4 partition for /"
-parted /dev/optiplex/$NAME mkpartfs primary ext2 4G 100%
+parted /dev/$VG/$NAME mkpartfs primary ext2 4G 100%
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Open LV"
-kpartx -a /dev/optiplex/template
-kpartx -a /dev/optiplex/$NAME
+kpartx -a /dev/$VG/template
+kpartx -a /dev/$VG/$NAME
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Import template / into the new /"
-dd bs=4M if=/dev/mapper/optiplex-template2 of="/dev/mapper/optiplex-"$NAME"2"
+dd bs=4M if="/dev/mapper/"$VG"-template2" of="/dev/mapper/"$VG"-"$NAME"2"
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Fixing new / filesystem"
-e2fsck -fy "/dev/mapper/optiplex-"$NAME"2"
+e2fsck -fy "/dev/mapper/"$VG"-"$NAME"2"
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Growing new / filesystem"
-resize2fs "/dev/mapper/optiplex-"$NAME"2"
+resize2fs "/dev/mapper/"$VG"-"$NAME"2"
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Mounting new filesystem"
 TEMP=`mktemp -d`
-mount "/dev/mapper/optiplex-"$NAME"2" $TEMP
+mount "/dev/mapper/"$VG"-"$NAME"2" $TEMP
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Updating hostname information"
@@ -102,8 +108,8 @@ rm -r $TEMP
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Close LV"
-kpartx -d /dev/optiplex/template
-kpartx -d /dev/optiplex/$NAME
+kpartx -d /dev/$VG/template
+kpartx -d /dev/$VG/$NAME
 next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating new configuration file"
@@ -111,7 +117,7 @@ echo "" > $CONFIG_PATH/$NAME.cfg
 cat >> $CONFIG_PATH/$NAME.cfg << EOF
 name = "$NAME"
 memory = 256
-disk = ["phy:/dev/optiplex/$NAME,xvda,w"]
+disk = ["phy:/dev/$VG/$NAME,xvda,w"]
 vif = [" "]
 bootloader = "pygrub"
 EOF

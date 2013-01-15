7a911bf43226db195bd2af24b4502de386b35b49
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-15 07:10:16 +0100

    put the 'next' operation into its own function
    ¡ not tested !

--- create_vm.sh
+++ create_vm.sh
@@ -47,73 +47,64 @@ case "undefined" in
 		;;
 esac;
 
+next(STATUS){
+    let CUR_STATE=$CUR_STATE+1
+    if [ $STATUS != 0 ]; then exit 1; fi
+}
 
 echo "$CUR_STATE/$TOT_STATE - Creating a new logical volume for the VM"
 lvcreate -L $SIZE -n $NAME optiplex
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating a partition table on the new LV"
 parted /dev/optiplex/$NAME mktable msdos
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating a swap partition"
 parted /dev/optiplex/$NAME mkpartfs primary linux-swap 1 4G
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
 
 echo "$CUR_STATE/$TOT_STATE - Creating a ext4 partition for /"
 parted /dev/optiplex/$NAME mkpartfs primary ext2 4G 100%
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Open LV"
 kpartx -a /dev/optiplex/template
 kpartx -a /dev/optiplex/$NAME
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Import template / into the new /"
 dd bs=4M if=/dev/mapper/optiplex-template2 of="/dev/mapper/optiplex-"$NAME"2"
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Fixing new / filesystem"
 e2fsck -fy "/dev/mapper/optiplex-"$NAME"2"
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Growing new / filesystem"
 resize2fs "/dev/mapper/optiplex-"$NAME"2"
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Mounting new filesystem"
 TEMP=`mktemp -d`
 mount "/dev/mapper/optiplex-"$NAME"2" $TEMP
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Updating hostname information"
 sed -i 's/template/'$NAME'/' $TEMP/etc/hostname
 sed -i 's/template/'$NAME'/' $TEMP/etc/hosts
 sed -i 's/template/'$NAME'/' $TEMP/etc/hosts
 # did it twice because template appear twice on a single line
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Unmounting new filesystem"
 umount $TEMP
 rm -r $TEMP
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Close LV"
 kpartx -d /dev/optiplex/template
 kpartx -d /dev/optiplex/$NAME
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Creating new configuration file"
 echo "" > $CONFIG_PATH/$NAME.cfg
@@ -124,12 +115,10 @@ disk = ["phy:/dev/optiplex/$NAME,xvda,w"]
 vif = [" "]
 bootloader = "pygrub"
 EOF
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "$CUR_STATE/$TOT_STATE - Starting the new VM"
 xm create $CONFIG_PATH/$NAME.cfg
-                                                                                let CUR_STATE=$CUR_STATE+1
-                                                                                if [ $? != 0 ]; then exit 1; fi
+next($?)
 
 echo "--- The END ---"
